478816357610c70178db7e8f63024fd1a7d78ad2
Timid_ <151643430+Timid05@users.noreply.github.com>
2025-05-08 11:08:54 +0200

    FPS Counter

---
+++ Assets/Scripts/TestView.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
+
+public class TestView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI fpsCount;
+    [SerializeField]
+    private float fpsInterval = 2f;
+    private int fpsSampleRate = 10;
+
+
+    float lastDisplayTime = 0;
+    float lastSampleTime = 0;
+    int[] sampleFPS;
+    int sampleCount = 0;
+    private void DisplayFPS()
+    {
+        if (lastSampleTime == 0)
+        {
+            sampleFPS = new int[fpsSampleRate];
+        }
+
+        if (Time.time - lastSampleTime >= fpsInterval / fpsSampleRate)
+        {
+            sampleFPS[sampleCount] = (int)(1.0f / Time.deltaTime);
+            lastSampleTime = Time.time;
+            sampleCount++;
+        }
+
+        if (Time.time - lastDisplayTime >= fpsInterval)
+        {
+            Debug.Log ("FPS given: " + string.Join (", ", sampleFPS));
+            float averageFPS = (int)sampleFPS.Average();
+            Debug.Log("calculated FPS: " + averageFPS);
+            fpsCount.text = "FPS: " + averageFPS;
+            lastDisplayTime = Time.time;
+            sampleCount = 0;
+        }
+    }
+
+
+    private void Update()
+    {
+        DisplayFPS();
+    }
+}
