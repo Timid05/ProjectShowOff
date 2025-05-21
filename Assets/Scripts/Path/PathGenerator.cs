178eac3ede294a93d3af6065ca082657b461ace2
Julius Struijk <julius.struijk@gmail.com>
2025-05-21 18:22:21 +0200

    Implemented Path Generation Logic

--- Assets/Scripts/Path/PathGenerator.cs
+++ Assets/Scripts/Path/PathGenerator.cs
@@ -10,12 +10,21 @@ public class PathGenerator : MonoBehaviour
     List<bool> pastDirections;
     bool generateRight;
 
+    [SerializeField] bool debugMode = true;
+    MeshRenderer mr;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Hide all waypoint pieces before generating. They are then revealed if they are part of the main path.
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
         pastDirections = new List<bool>();
-        // This allows the path to start generating properly.
-        prevWaypoint = startWaypoint.SouthWaypoint;
+        // This sets the previous waypoint to south, which allows the path to start generating properly.
+        prevWaypoint = startWaypoint.waypoints.Keys[1];
         currentWaypoint = startWaypoint;
         //GeneratePath(startWaypoint);
     }
@@ -31,86 +40,100 @@ public class PathGenerator : MonoBehaviour
     void GeneratePath()
     {
         Debug.Log("Generating for waypoint: " + currentWaypoint.name);
-        currentWaypoint.GetComponent<MeshRenderer>().enabled = true;
+        // Make waypoint visible if it chosen.
+        currentWaypoint.gameObject.SetActive(true);
+        mr = currentWaypoint.GetComponent<MeshRenderer>();
+        mr.enabled = true;
 
-        // Having randomisation be a seperate function allows for a more concrete variable in this function.
-        generateRight = RandomisePathDirection();
-        Debug.Log("Going right? " + generateRight);
+        //On the second to last waypoint the only option is to go to the ending, so if there's only one waypoint then everything can be skipped.
+        if (currentWaypoint.waypoints.Count == 1) {
+            Debug.Log("Second to last waypoint reached.");
+            currentWaypoint = currentWaypoint.waypoints.Keys[0];
+            //Make end waypoint visible as well.
+            currentWaypoint.gameObject.SetActive(true);
+            mr = currentWaypoint.GetComponent<MeshRenderer>();
+            mr.enabled = true;
+            return; 
+        }
 
-        //Determine which waypoints are left and right based on which direction the path is coming from. If it is unknown, default to west being left and east being right.
+        //Determine which waypoints are left and right based on which direction the path is coming from.
         if (prevWaypoint != null)
         {
-            List<PathWaypoint> waypoints = new List<PathWaypoint> { currentWaypoint.NorthWaypoint, currentWaypoint.SouthWaypoint, currentWaypoint.WestWaypoint, currentWaypoint.EastWaypoint };
-            // This will have the waypoints that the next one will be chosen from.
-            List<PathWaypoint> possibleNextWaypoints = new List<PathWaypoint>();
-            //Debug.Log("Waypoint count: " + waypoints.Count);
-            for (int i = 0; i < waypoints.Count; i++)
+            //This will have the waypoints that the next one will be chosen from.
+            UDictionary<PathWaypoint, bool> possibleNextWaypoints = new UDictionary<PathWaypoint, bool>();
+            bool swapResults = false;
+            for (int i = 0; i < currentWaypoint.waypoints.Count; i++)
             {
-                if (prevWaypoint == waypoints[i])
+                if (prevWaypoint == currentWaypoint.waypoints.Keys[i])
                 {
-                    waypoints.RemoveAt(i);
-                    // Add the waypoints perpendicular to the direction the path is coming from.
-                    if (prevWaypoint == currentWaypoint.NorthWaypoint || prevWaypoint == currentWaypoint.SouthWaypoint)
-                    {
-                        possibleNextWaypoints.Add(currentWaypoint.WestWaypoint);
-                        possibleNextWaypoints.Add(currentWaypoint.EastWaypoint);
-                    }
-                    else
-                    {
-                        possibleNextWaypoints.Add(currentWaypoint.NorthWaypoint);
-                        possibleNextWaypoints.Add(currentWaypoint.SouthWaypoint);
+                    //Swap the results of left and right if the waypoints orientation is North of East, so it matches their viewpoint.
+                    if (prevWaypoint == currentWaypoint.waypoints.Keys[0] || prevWaypoint == currentWaypoint.waypoints.Keys[3]) { swapResults = true;
+                        //Debug.Log("Swapping results!");
                     }
 
-                    //Remove possible waypoints from the original list. This will allow the option to add a backup option if necessary.
-                    waypoints.Remove(possibleNextWaypoints[0]);
-                    waypoints.Remove(possibleNextWaypoints[1]);
+                    // We set the i, so that it adds the waypoints perpendicular to the direction the path is coming from.
+                    if (i <= 1){ i = 2; }
+                    else{ i = 0; }
+                    possibleNextWaypoints.Add(currentWaypoint.waypoints.Keys[i], currentWaypoint.waypoints.Values[i]);
+                    possibleNextWaypoints.Add(currentWaypoint.waypoints.Keys[i+1], currentWaypoint.waypoints.Values[i+1]);
+
+                    //Remove current waypoint and possible waypoints from the original dict. This will allow the option to add a backup option if necessary.
+                    currentWaypoint.waypoints.Keys.Remove(prevWaypoint);
+                    currentWaypoint.waypoints.Remove(possibleNextWaypoints.Keys[0]);
+                    currentWaypoint.waypoints.Remove(possibleNextWaypoints.Keys[1]);
                     break;
                 }
             }
             //Check if one of the possible waypoints is missing.
             for (int i = 0; i < possibleNextWaypoints.Count; i++)
             {
-                if (possibleNextWaypoints[i] == null)
+                if (possibleNextWaypoints.Keys[i] == null)
                 {
-                    //Debug.LogFormat("Replacing missing waypoint at index {0} with {1}", i, waypoints[0]);
-                    //If it is missing, replace it with the one remaining waypoint in the original list.
-                    possibleNextWaypoints[i] = waypoints[0];
+                    //Debug.LogFormat("Replacing missing waypoint at index {0} with {1}", i, currentWaypoint.waypoints.Keys[0]);
+                    //If it is missing, replace it with the one remaining waypoint in the original dict. If there are no re 
+                    possibleNextWaypoints.Keys[i] = currentWaypoint.waypoints.Keys[0];
+                    possibleNextWaypoints.Values[i] = currentWaypoint.waypoints.Values[0];
                     break;
                 }
             }
 
-
-            //Swap the results of left and right if the waypoints orientation is North of East, so it matches their viewpoint.
-            bool swapResults = false;
-            if (prevWaypoint == currentWaypoint.NorthWaypoint || prevWaypoint == currentWaypoint.EastWaypoint) { swapResults = true; }
-
-            if (swapResults) { DecideNextWaypoint(possibleNextWaypoints[1], possibleNextWaypoints[0]); }
-            else { DecideNextWaypoint(possibleNextWaypoints[0], possibleNextWaypoints[1]); }
+            //Debug.LogFormat("Possible waypoints {0} and {1}.", possibleNextWaypoints.Keys[0].name, possibleNextWaypoints.Keys[1].name);
+            if (swapResults) { DecideNextWaypoint(new KeyValuePair<PathWaypoint, bool>(possibleNextWaypoints.Keys[1], possibleNextWaypoints.Values[1]), new KeyValuePair<PathWaypoint, bool>(possibleNextWaypoints.Keys[0], possibleNextWaypoints.Values[0])); }
+            else { DecideNextWaypoint(new KeyValuePair<PathWaypoint, bool>(possibleNextWaypoints.Keys[0], possibleNextWaypoints.Values[0]), new KeyValuePair<PathWaypoint, bool>(possibleNextWaypoints.Keys[1], possibleNextWaypoints.Values[1])); }
         }
-        else { DecideNextWaypoint(currentWaypoint.WestWaypoint, currentWaypoint.EastWaypoint); }
+        else { DecideNextWaypoint(new KeyValuePair<PathWaypoint, bool>(currentWaypoint.waypoints.Keys[2], currentWaypoint.waypoints.Values[2]), new KeyValuePair<PathWaypoint, bool>(currentWaypoint.waypoints.Keys[3], currentWaypoint.waypoints.Values[3])); ; }
     }
 
-    void DecideNextWaypoint(PathWaypoint leftWaypoint, PathWaypoint rightWaypoint)
+    void DecideNextWaypoint(KeyValuePair<PathWaypoint, bool> leftPair, KeyValuePair<PathWaypoint, bool> rightPair)
     {
-        Debug.LogFormat("Choosing next waypoint between {0} and {1}.", leftWaypoint.name, rightWaypoint.name);
+        Debug.LogFormat("Choosing next waypoint between {0} and {1}.", leftPair.Key.name, rightPair.Key.name);
 
-        // If one of the options has already been visited, the direction needs to be forced the other way.
-        if(!leftWaypoint.enabled)
+        // If one of the options has already been visited or the path is disabled, the direction needs to be forced the other way.
+        if(!leftPair.Key.enabled || !leftPair.Value)
         {
-            Debug.Log("Forcing direction right");
+            Debug.LogFormat("Forcing direction right. Key status: {0} Path status: {1}", leftPair.Key.enabled, leftPair.Value);
             generateRight = true;
         }
-        else if(!rightWaypoint.enabled)
+        else if (!rightPair.Key.enabled || !rightPair.Value)
         {
-            Debug.Log("Forcing direction left");
+            Debug.LogFormat("Forcing direction left. Key status: {0} Path status: {1}", rightPair.Key.enabled, rightPair.Value);
             generateRight = false;
         }
+        // If the direction isn't forced we randomise it.
+        else
+        {
+            generateRight = RandomisePathDirection();
+            Debug.Log("Going right? " + generateRight);
+        }
+
         // Add the direction to the list after any potential forcing, so the accurate version gets added.
         pastDirections.Add(generateRight);
+        // If Debug is enabled, change the material to add a visual indication of which direction the path generated
+        if (debugMode && generateRight) { mr.material.color = Color.green; }
 
         prevWaypoint = currentWaypoint;
-        if (generateRight) { currentWaypoint = rightWaypoint; }
-        else { currentWaypoint = leftWaypoint; }
+        if (generateRight) { currentWaypoint = rightPair.Key; }
+        else { currentWaypoint = leftPair.Key; }
         // Disable previous waypoint to note that it's already been visited.
         prevWaypoint.enabled = false;
     }
@@ -120,7 +143,7 @@ public class PathGenerator : MonoBehaviour
         // Restrict direction if the past two directions have been the same.
         if (pastDirections.Count >= 2 && pastDirections[pastDirections.Count - 1] == pastDirections[pastDirections.Count - 2])
         {
-            Debug.Log("Forcing direction.");
+            Debug.Log("2 of the same results in a row, forcing direction.");
             // Force the path to go in the opposite direction than the past two directions.
             return !pastDirections[pastDirections.Count - 1];
         }
--- Assets/Scripts/Path/PathWaypoint.cs
+++ Assets/Scripts/Path/PathWaypoint.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PathWaypoint : MonoBehaviour
 {
     [field: SerializeField] public bool EndWaypoint { get; private set; } = false;
-    [field: SerializeField] public PathWaypoint NorthWaypoint { get; private set; }
-    [field: SerializeField] public PathWaypoint SouthWaypoint { get; private set; }
-    [field: SerializeField] public PathWaypoint WestWaypoint { get; private set; }
-    [field: SerializeField] public PathWaypoint EastWaypoint { get; private set; }
+
+    [field: SerializeField] public UDictionary<PathWaypoint, bool> waypoints { get; private set; }
 }
