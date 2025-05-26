ef201acebf297f54cf32d2ac88a8b7a787cb3f63
Julius Struijk <julius.struijk@gmail.com>
2025-05-26 16:23:41 +0200

    Started on Building Placement Logic and Fixed Path Generation Bug
    The bug was that the path was generating to waypoints that had no valid waypoints afterwards.

---
+++ Assets/Scripts/BuildingSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    [SerializeField] UDictionary<CapsuleCollider, GameObject> spawnableBuildings;
+
+    private void Awake()
+    {
+        PathGenerator.OnPathGenerated += PlaceObject;
+    }
+
+
+    //TODO: Connect to delegate that fires once path generation is done
+    // Place the object so that it is touching a road piece.
+    void PlaceObject()
+    {
+        Debug.Log("Placing objects.");
+        if(spawnableBuildings == null){ return; }
+        foreach(KeyValuePair<CapsuleCollider, GameObject> pair in spawnableBuildings)
+        {
+            if(pair.Key == null || pair.Value == null) { continue; }
+            Debug.LogFormat("Spawning object {0} in area {1}", pair.Value.name, pair.Key.name);
+        }
+    }
+
+    // Adjust the placement of the object so that it's next to the road instead of inside of it.
+    void AdjustObjectPlacement()
+    {
+
+    }
+
+    // Rotate object to face the road it's closest to.
+    void RotateObject()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        PathGenerator.OnPathGenerated -= PlaceObject;
+    }
+}
