4f99ccd4e762b598528463789d5854e9c5391f8c
Julius Struijk <julius.struijk@gmail.com>
2025-06-02 14:03:41 +0200

    Building Spawner now has main path pieces

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -8,20 +8,26 @@ public class BuildingSpawner : MonoBehaviour
 
     private void Awake()
     {
-        PathGenerator.OnPathGenerated += PlaceObject;
+        //PathGenerator.OnPathGenerated += PlaceObject;
+        ShareMainPath.OnMainPathOnly += PlaceObject;
+
     }
 
 
     // Place the object so that it is touching a road piece.
-    void PlaceObject()
+    void PlaceObject(List<GameObject> mainPathObjects, CapsuleCollider area)
     {
-        //Debug.Log("Placing objects.");
-        if(spawnableBuildings == null){ return; }
-        foreach(KeyValuePair<CapsuleCollider, GameObject> pair in spawnableBuildings)
-        {
-            if(pair.Key == null || pair.Value == null) { continue; }
-            //Debug.LogFormat("Spawning object {0} in area {1}", pair.Value.name, pair.Key.name);
-        }
+        if(spawnableBuildings == null || !spawnableBuildings.ContainsKey(area)){ return; }
+
+        spawnableBuildings.TryGetValue(area, out GameObject spawnObject);
+        Debug.LogFormat("Placing object {0} in area {1}", spawnObject.name, area.name);
+
+        //foreach(KeyValuePair<CapsuleCollider, GameObject> pair in spawnableBuildings)
+        //{
+        //    if(pair.Key == null || pair.Value == null) { continue; }
+        //    Debug.LogFormat("Spawning object {0} in area {1}", pair.Value.name, pair.Key.name);
+
+        //}
     }
 
     // Adjust the placement of the object so that it's next to the road instead of inside of it.
@@ -38,6 +44,7 @@ public class BuildingSpawner : MonoBehaviour
 
     private void OnDestroy()
     {
-        PathGenerator.OnPathGenerated -= PlaceObject;
+        //PathGenerator.OnPathGenerated -= PlaceObject;
+        ShareMainPath.OnMainPathOnly -= PlaceObject;
     }
 }
---
+++ Assets/Scripts/Path/ShareMainPath.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShareMainPath : MonoBehaviour
+{
+    public static event Action<List<GameObject>, CapsuleCollider> OnMainPathOnly;
+    List<GameObject> mainPathObjectsInArea;
+
+    private void Awake()
+    {
+        PathGenerator.OnPathGenerated += RemoveNonMainPathObjects;
+    }
+
+    private void Start()
+    {
+        mainPathObjectsInArea = new List<GameObject>();
+    }
+
+    // Reduce list of path objects in the area to only the ones on the main path, once generation is done.
+    private void RemoveNonMainPathObjects()
+    {
+        Debug.LogFormat("Area {0} has {1} path objects.", gameObject.name, mainPathObjectsInArea.Count);
+        for (int i = mainPathObjectsInArea.Count - 1; i >= 0; i--)
+        {
+            if(!mainPathObjectsInArea[i].activeSelf) { mainPathObjectsInArea.RemoveAt(i); }
+            else { Debug.LogFormat("Area {0} path object {1} is part of the main trail.", gameObject.name, mainPathObjectsInArea[i].name); }
+        }
+        Debug.LogFormat("Area {0} has {1} main path objects.", gameObject.name, mainPathObjectsInArea.Count);
+
+        // Share main path objects with building spawner and the area it originates from.
+        CapsuleCollider area = gameObject.GetComponent<CapsuleCollider>();
+        if(OnMainPathOnly != null && area != null) { OnMainPathOnly(mainPathObjectsInArea, area); }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Add all path objects in the area to the list.
+        if (other.CompareTag("Path"))
+        {
+            //Debug.LogFormat("Area {0} has path object {1}", gameObject.name, other.gameObject.name);
+            GameObject path = other.gameObject;
+
+            if (path != null) { mainPathObjectsInArea.Add(path); }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        PathGenerator.OnPathGenerated -= RemoveNonMainPathObjects;
+    }
+}
