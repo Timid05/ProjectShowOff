3e5523e09a47b576655da4107307eef159ba8560
Timid_ <151643430+Timid05@users.noreply.github.com>
2025-06-11 21:55:19 +0200

    Hellhound animations implemented

--- Assets/Scripts/Hellhound/HellhoundAnimations.cs
+++ Assets/Scripts/Hellhound/HellhoundAnimations.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,20 @@ using UnityEngine;
 public class HellhoundAnimations : MonoBehaviour
 {
     Animator animator;
+    AnimationClip currentClip;
+    public static Action OnPounceAnimDone;
+
+    private void OnEnable()
+    {
+        HellhoundActions.OnCharge += TriggerCharge;
+        HellhoundActions.OnPounce += TriggerPounce;
+    }
+
+    private void OnDisable()
+    {
+        HellhoundActions.OnCharge += TriggerCharge;
+        HellhoundActions.OnPounce += TriggerPounce;
+    }
 
     private void Awake()
     {
@@ -13,17 +28,39 @@ public class HellhoundAnimations : MonoBehaviour
 
     private void TriggerCharge()
     {
+        animator.SetBool("pouncing", false);
         animator.SetBool("charging", true);
     }
 
     private void TriggerPounce()
     {
+        animator.SetBool("charging", false);
         animator.SetBool("pouncing", true);
+        StartCoroutine(Pouncing());
+    }
+
+    private IEnumerator Pouncing()
+    {
+        Debug.Log("Waiting till end");
+        yield return new WaitForSeconds(GetCurrenStateInfo().length + 0.1f);
+        Debug.Log("Invoking pounce end");
+        OnPounceAnimDone?.Invoke();
+        StopAnimating();
     }
 
     private void StopAnimating()
     {
         animator.SetBool("charging", false);
         animator.SetBool("pouncing", false);
+    }
+
+    AnimatorClipInfo GetCurrentClipInfo()
+    {
+        return animator.GetCurrentAnimatorClipInfo(0)[0];
+    }
+
+    AnimatorStateInfo GetCurrenStateInfo()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0);  
     }
 }
