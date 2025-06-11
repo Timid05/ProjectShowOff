84fa9647d2a2f7dadec23982eadedc1161810dfb
Julius Struijk <julius.struijk@gmail.com>
2025-06-11 13:58:03 +0200

    Working on Object Put Down

--- Assets/Scripts/Player/PlayerObject.cs
+++ Assets/Scripts/Player/PlayerObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerObject : MonoBehaviour
 {
     Camera _camera;
-    [SerializeField] GameObject carriedObject;
+    [SerializeField] GameObject carriedObjectPosition;
     bool carryingObject = false;
     void Start()
     {
@@ -14,15 +14,22 @@ public class PlayerObject : MonoBehaviour
 
     void Update()
     {
-        if (!carryingObject && carriedObject != null && Input.GetMouseButtonUp(0) && Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hitinfo)) 
+        if(Input.GetMouseButtonUp(0))
         {
-            if(hitinfo.collider.gameObject.CompareTag("PickUpObject"))
+            // Pickup Object
+            if (!carryingObject && carriedObjectPosition != null && Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hitinfo))
             {
-                // Pickup Object
-                carryingObject = true;
-                hitinfo.collider.gameObject.transform.position = carriedObject.transform.position;
-                hitinfo.collider.gameObject.transform.parent = _camera.transform;
+                if (hitinfo.collider.gameObject.CompareTag("PickUpObject"))
+                {
+                    carryingObject = true;
+                    hitinfo.collider.gameObject.transform.position = carriedObjectPosition.transform.position;
+                    hitinfo.collider.gameObject.transform.parent = _camera.transform;
+                }
             }
+
+            // Put down Object
+
         }
+
     }
 }
