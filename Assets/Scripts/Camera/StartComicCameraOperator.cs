1a05a5fed8a1e0209ee9b15b9a0828eaf19268f7
froo <marius.a.irimia@gmail.com>
2025-07-03 14:04:47 +0200

    TREES FIXED FOR BUILD

--- Assets/Scripts/Camera/StartComicCameraOperator.cs
+++ Assets/Scripts/Camera/StartComicCameraOperator.cs
@@ -2,7 +2,7 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Build.Content;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
